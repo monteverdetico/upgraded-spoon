bd9160dd27fa3f39e5ac2a1b2e041817179d8f1f
Marcelo Guindon <marcelo.guindon@gmail.com>
2024-06-13 23:50:23 +0200

    FEATURE: Add ability to search for your recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,10 +2,20 @@
 
 class RecipesController < ApplicationController
   def index
-    @pagy, @recipes = pagy(Recipe.all)
+    @pagy, @recipes = pagy(recipes)
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipes
+    if params[:query].blank?
+      Recipe.all
+    else
+      Recipe.search(params[:query])
+    end
   end
 end
